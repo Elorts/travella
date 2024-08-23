9570e83f381effa4a637f4d124a3352157a68404
deividas strole <deiwis@yahoo.com>
2024-08-22 21:21:12 -0700

    new code

---
+++ src/main/java/com/visitormaker/travella/TravellaApplication.java
@@ -0,0 +1,13 @@
+package com.visitormaker.travella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visitormaker/travella/TravellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visitormaker.travella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
